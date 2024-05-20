41f7f5798f11e1d8ac521de094492f0c83fa79d2
Nosalis1 <aleksa9192@gmail.com>
2024-05-20 09:42:24 +0200

    Setup

--- src/main/java/metropolitan/imageeditor/App.java
+++ src/main/java/metropolitan/imageeditor/App.java
@@ -3,16 +3,21 @@ package metropolitan.imageeditor;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class App extends Application {
+
+    public static final int WINDOW_WIDTH = 1280;
+    public static final int WINDOW_HEIGHT = 720;
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Pane root = new Pane();
+        Scene scene = new Scene(root, WINDOW_WIDTH, WINDOW_HEIGHT);
+        stage.setTitle("Image Editor");
         stage.setScene(scene);
         stage.show();
     }
